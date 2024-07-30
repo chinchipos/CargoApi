47341bb36598ce0cb6025e827fb7b5cbed9fa3c3
chinchipos <chinchipos1984@gmail.com>
2024-07-31 04:43:43 +0600

    Code updated

--- src/services/transaction.py
+++ src/services/transaction.py
@@ -2,6 +2,7 @@ from datetime import date, timedelta, datetime
 from dateutil.relativedelta import relativedelta
 from typing import List
 
+from src.config import TZ
 from src.database.model.models import Transaction as TransactionOrm
 from src.repositories.transaction import TransactionRepository
 
@@ -15,10 +16,10 @@ class TransactionService:
     async def get_transactions(self, company_id: str | None, from_dt: datetime | None, to_dt: datetime | None) \
             -> List[TransactionOrm]:
         if not from_dt:
-            from_dt = date.today() - relativedelta(months = 3 if company_id else 1)
+            from_dt = datetime.now(tz=TZ).date() - relativedelta(months = 3 if company_id else 1)
 
         if not to_dt:
-            to_dt = date.today() + timedelta(days=1)
+            to_dt = datetime.now(tz=TZ).date() + timedelta(days=1)
 
         transactions = await self.repository.get_transactions(company_id, from_dt, to_dt)
         return transactions
