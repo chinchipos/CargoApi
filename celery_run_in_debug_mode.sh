0677a06097a6a8ba26f4b2505115d44ae328419d
Anton <a.mamykin@cargonomica.com>
2024-09-11 06:37:34 +0600

    Code updated

--- celery_run_in_debug_mode.sh
+++ celery_run_in_debug_mode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 source /home/cargo/env/bin/activate
-cd /home/cargo/cargonomica/api
+cd /home/cargo/cargonomica/api || exit
 celery -A src.celery_app.main:celery purge -f
-celery -A src.celery_app.main:celery worker --loglevel=ERROR
+celery -A src.celery_app.main:celery worker --loglevel=INFO
 deactivate
