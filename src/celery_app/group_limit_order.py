463948da93a5efaf2c4ccc15d490d1562e1f4fb4
Anton <a.mamykin@cargonomica.com>
2024-09-11 09:00:26 +0600

    Code updated

--- src/celery_app/group_limit_order.py
+++ src/celery_app/group_limit_order.py
@@ -3,10 +3,15 @@ from src.database.models import GroupLimitOrm, CompanyOrm
 from src.database.models.goods_category import GoodsCategory
 
 
-class GroupLimitOrder:
+class GroupLimitOrder(dict):
 
     def __init__(self, personal_account: str, delta_sum: float = 0):
         self.personal_account = personal_account
         self.delta_sum = delta_sum
         self.company: CompanyOrm | None = None
         self.local_group_limits: List[GroupLimitOrm] = []
+        dict.__init__(
+            self,
+            personal_account=self.personal_account,
+            delta_sum=self.delta_sum
+        )
