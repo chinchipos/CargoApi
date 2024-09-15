8c384a7436608a544cf53f979027535c1236fff7
Anton <a.mamykin@cargonomica.com>
2024-09-15 11:31:40 +0600

    Code updated

--- src/celery_app/khnp/tasks.py
+++ src/celery_app/khnp/tasks.py
@@ -57,7 +57,7 @@ def khnp_set_card_states(balance_ids_to_change_card_states: Dict[str, List[str]]
         if sys.platform == 'win32':
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-        asyncio.run(khnp_set_card_states_fn(balance_ids_to_change_card_states))
+        # asyncio.run(khnp_set_card_states_fn(balance_ids_to_change_card_states))
 
     else:
         _logger.info('Блокировка / разблокировка карт не требуется: '
