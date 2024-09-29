ede6dfb59846bfe28c2c2de46c28d2514fd3b6e0
Anton <a.mamykin@cargonomica.com>
2024-09-29 11:28:37 +0600

    Code updated

--- src/celery_app/gpn/api.py
+++ src/celery_app/gpn/api.py
@@ -40,10 +40,10 @@ class GpnGoodsCategory(Enum):
     def get_equal_by_local(local_category: GoodsCategory):
         for gpn_category in GpnGoodsCategory:
             print('PPPPPPPPPPPPPPPPPPPPPP')
-            print(gpn_category.value["local_category"])
-            print(local_category)
-            print(gpn_category.value["local_category"] == local_category)
-            if gpn_category.value["local_category"] == local_category:
+            print(gpn_category.value["local_category"].name)
+            print(local_category.name)
+            print(gpn_category.value["local_category"].name == local_category.name)
+            if gpn_category.value["local_category"].name == local_category.name:
                 return gpn_category
 
 
